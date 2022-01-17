1506e834d54a9d9e5ace541e1007857f02f11f4f
Heiko Wobst <wobst.heiko1010@t-online.de>
2022-01-17 18:26:41 +0100

    version to reproduce issue #15066

---
+++ XamarinFormsLayoutChangesBug/XamarinFormsLayoutChangesBug.UWP/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace XamarinFormsLayoutChangesBug.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            LoadApplication(new XamarinFormsLayoutChangesBug.App());
+        }
+    }
+}
---
+++ XamarinFormsLayoutChangesBug/XamarinFormsLayoutChangesBug/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace XamarinFormsLayoutChangesBug
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            ContainerStackLayout.Children.Add(new StackLayout() { BackgroundColor = Color.Gray, HeightRequest = 40.0 });
+        }
+    }
+}
